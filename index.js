80ae0b857148fbb9962a07cb79dfd3ebae8c9a93
manhkkkk <pktopad@gmail.com>
2022-09-14 21:48:18 +0700

    update

---
+++ index.js
@@ -0,0 +1,20 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('./db.json');
+const cors = require('cors');
+const middlewares = jsonServer.defaults({
+  static: './build'
+});
+const PORT = process.env.PORT || 4000;
+server.use(cors());
+server.options('*', cors());
+server.use(middlewares);
+server.use(jsonServer.rewriter({
+  '/api/*': '/$1',
+}))
+
+server.use(router);
+server.listen(PORT, () => {
+  console.log('Server is running');
+});
+
